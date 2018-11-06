4ff1466e422bf1998205b5e0d226abf3906e0a13
Carolina <carolinamalbuquerque29@gmail.com>
2018-11-06 12:04:11 +0000

    Firebase Authentication

---
+++ app/src/main/java/icm/greencities/CreateAccount.java
@@ -0,0 +1,16 @@
+package icm.greencities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+    }
+}
