12edf58412fbc4053bfcd2332ec4bca66e6ec025
Carolina <carolinamalbuquerque29@gmail.com>
2018-11-06 12:23:50 +0000

    Firebase Authentication DOne

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -66,11 +66,11 @@ public final class R {
         public static final int adjust_width = 0x7f08001c;
         public static final int auto = 0x7f080021;
         public static final int dark = 0x7f08003a;
-        public static final int icon_only = 0x7f080061;
-        public static final int light = 0x7f08006c;
-        public static final int none = 0x7f08007e;
-        public static final int standard = 0x7f0800ba;
-        public static final int wide = 0x7f0800da;
+        public static final int icon_only = 0x7f08005d;
+        public static final int light = 0x7f080068;
+        public static final int none = 0x7f080078;
+        public static final int standard = 0x7f0800b3;
+        public static final int wide = 0x7f0800d2;
     }
     public static final class string {
         private string() {}
